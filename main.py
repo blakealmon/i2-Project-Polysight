74d7f0800b710131598d453791e31ffbb489a734
BlakeAlmon <all.business.blake@gmail.com>
2024-04-13 10:44:04 -0700

    changes

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import streamlit as st
 import openai
 import trafilatura
 
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
 
 st.title(':blue[Polysight]' + " [Website still being worked on]")
 
-url = st.text_input('Enter URL')
+url = st.text_input('Enter URL - Takes some time (Make sure to press enter)')
 
 
 #url = 'https://www.cnn.com/2024/04/07/politics/supreme-court-5th-circuit/index.html'
@@ -13,18 +17,20 @@ downloaded = trafilatura.fetch_url(url)
 article_content = trafilatura.extract(downloaded)
 print(article_content)
 
-# gif
-#st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
+
 
 if article_content is None:
     article_content = ""
 else:
-    # Define your OpenAI API key
 
+    image = st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcm9ndGR3cjM1cnpoMzR5dzNuYzFlMG4xdHA1b25rdXM0azlldjMxdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/zlcIBNopQj8Yx5QgpR/giphy.gif)")
+
+    # Define your OpenAI API key
     openai.api_key = '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'
 
     response = openai.ChatCompletion.create( model="gpt-4", messages=[{"role": "user", "content": "You are to be given an article and then you must first say if the article leans to the right or the left or neutral politically then give it a rating from 0.00 - 1.00 on how biased it is toward the side and include what the bias is by saying 'I would rate it [bias] on it being leaning to the direction politically (1.00 being max)' (you must paraphrase). [DO NOT USE I or any pronouns and sound robotic] Then take a deep breath and give reasoning behind your choice. Do not make the reasoning long. Make sure the reasoning is like around 2 sentences and in bullet points. Make sure that there is a line indent between reasoning and bias. and Here is the article : " + article_content}] ) # print(response)
 
+ 
 
     x = "" + response.choices[0].message.content
     st.write(x)
---
+++ pages/sidebar.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Info",
+)
+
+st.write("Project was made by Blake Almon for i2")
+st.write("Project was made by Blake Almon")
