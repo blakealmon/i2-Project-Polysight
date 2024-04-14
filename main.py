3fff495a659c52c61a55dd920c564d7a90158ad6
BlakeAlmon <all.business.blake@gmail.com>
2024-04-14 10:42:38 -0700

    updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import openai
 import trafilatura
 
+
 st.set_page_config(
     page_title="Home",
     page_icon="👋",
@@ -23,10 +24,10 @@ if article_content is None:
     article_content = ""
 else:
 
-    image = st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcm9ndGR3cjM1cnpoMzR5dzNuYzFlMG4xdHA1b25rdXM0azlldjMxdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/zlcIBNopQj8Yx5QgpR/giphy.gif)")
+    #image = st.markdown("![Alt Text](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcm9ndGR3cjM1cnpoMzR5dzNuYzFlMG4xdHA1b25rdXM0azlldjMxdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/zlcIBNopQj8Yx5QgpR/giphy.gif)")
 
     # Define your OpenAI API key
-    openai.api_key = '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'
+    openai.api_key = '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'
 
     response = openai.ChatCompletion.create( model="gpt-4", messages=[{"role": "user", "content": "You are to be given an article and then you must first say if the article leans to the right or the left or neutral politically then give it a rating from 0.00 - 1.00 on how biased it is toward the side and include what the bias is by saying 'I would rate it [bias] on it being leaning to the direction politically (1.00 being max)' (you must paraphrase). [DO NOT USE I or any pronouns and sound robotic] Then take a deep breath and give reasoning behind your choice. Do not make the reasoning long. Make sure the reasoning is like around 2 sentences and in bullet points. Make sure that there is a line indent between reasoning and bias. and Here is the article : " + article_content}] ) # print(response)
 
@@ -38,7 +39,3 @@ else:
 
 
 
-st.write(":blue[Project was made by Blake Almon for i2]")
-
-url = "https://github.com/blakealmon"
-st.write("Github : [link](%s)" % url)
